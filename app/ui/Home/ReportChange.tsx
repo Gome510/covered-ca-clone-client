44970e9e1e949493d9ced1eee33d62d7b767a051
Gome510 <bluehawkfire55@gmail.com>
2024-04-09 17:05:30 -0700

    report change prog

--- app/ui/Home/ReportChange.tsx
+++ app/ui/Home/ReportChange.tsx
@@ -1,10 +1,12 @@
 function ReportChange() {
   return (
-    <div className="flex flex-col items-center">
-      <h2>Need to Report a Change?</h2>
-      <p>
+    <div className="flex flex-col items-center gap-8">
+      <h2 className="mt-12">Need to Report a Change?</h2>
+      <p className="text-center px-8">
         Not sure? Watch the video below or{" "}
-        <a href=""> click here to learn more.</a>
+        <a className="text-primary underline" href="">
+          click here to learn more.
+        </a>
       </p>
       <iframe
         className="rounded-xl"
@@ -12,11 +14,21 @@ function ReportChange() {
         height="208"
         src="https://www.youtube.com/embed/-d6qoR5N7GM?si=EYOMW8zfiXru1icY"
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
+
+      <a
+        className=" bg-primary text-white text-lg font-semibold h-16 w-44 rounded-lg flex items-center justify-center"
+        href=""
+      >
+        Report Online
+      </a>
+
+      <p>or</p>
+
+      <p className="">Call: (800) 300-1506</p>
     </div>
   );
 }
