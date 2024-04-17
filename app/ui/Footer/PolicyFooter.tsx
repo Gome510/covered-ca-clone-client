53ca3d0278db61ab158f469ca5619db939ff1cf9
Gome510 <bluehawkfire55@gmail.com>
2024-04-17 16:47:23 -0700

    [feat] Style Policy Footer

--- app/ui/Footer/PolicyFooter.tsx
+++ app/ui/Footer/PolicyFooter.tsx
@@ -1,15 +1,16 @@
+import "./policyfooter.css";
 function PolicyFooter() {
   return (
-    <section>
-      <div className="flex">
+    <section className="px-4 text-xs">
+      <div className="flex flex-col items-center  gap-2 mt-8">
         <a href="">Accessibility and Nondiscrimination</a>
         <a href="">Terms of Use</a>
         <a href="">Privacy Policy</a>
         <a href="">Protecting Our Consumers</a>
       </div>
-      <hr />
+      <hr className="border-slate-gray" />
 
-      <ul id="language-links">
+      <ul id="language-links" className="flex flex-wrap gap-2 justify-center">
         <li>
           <a href="/espanol/">Español</a>
         </li>
@@ -70,7 +71,9 @@ function PolicyFooter() {
         that&apos;s right for them
       </p>
 
-      <p>Copyright © 2024 Covered California</p>
+      <p className="mt-4 text-center mb-8">
+        Copyright © 2024 Covered California
+      </p>
     </section>
   );
 }
