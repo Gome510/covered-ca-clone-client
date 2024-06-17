1ac11f8c62b1b652fc07436b702b4a7afb8bf941
Gome510 <bluehawkfire55@gmail.com>
2024-06-17 10:40:35 -0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen text-center pt-8">
-      <h2>Not Found</h2>
-      <p className="mb-4">Could not find requested resource</p>
-      <Link
-        className="bg-primary font-bold text-white text-center rounded-full py-1 px-4 w-40"
-        href="/"
-      >
-        Return Home
-      </Link>
-    </div>
+    <html>
+      <body>
+        <div className="min-h-screen text-center pt-8">
+          <h2>Not Found</h2>
+          <p className="mb-4">Could not find requested resource</p>
+          <Link
+            className="bg-primary font-bold text-white text-center rounded-full py-1 px-4 w-40"
+            href="/"
+          >
+            Return Home
+          </Link>
+        </div>
+      </body>
+    </html>
   );
 }
